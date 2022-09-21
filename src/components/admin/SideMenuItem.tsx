d20da3a394ff0eeb82fd66ae3f8e072a58804b73
Parallyz <vladislav.verus@gmail.com>
2022-09-21 19:38:49 +0300

    activemenu done

--- src/components/admin/SideMenuItem.tsx
+++ src/components/admin/SideMenuItem.tsx
@@ -1,31 +1,30 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 import { adminPath } from "../../models/models";
 
 type SideMenuItemProps = {
-   item: adminPath;
-  
+  item: adminPath;
 };
 const SideMenuItem = ({ item }: SideMenuItemProps) => {
- 
-
-  function clickHandler(event: React.MouseEvent<HTMLAnchorElement>) {
-    document.querySelectorAll(".sidemenu__link").forEach((e) => {
-      e.classList.remove("sidemenu__link--active");
-    });
-
-    event.currentTarget.classList.add("sidemenu__link--active");
-  }
   return (
     <>
       <div className="sidemenu__item " key={item.path}>
-        <Link to={item.path} className="sidemenu__link " onClick={clickHandler}>
-          <div className="sidemenu__img">
-            <img src={item.icon} />
-          </div>
-          <div className="sidemenu__text">{item.name}</div>
-        </Link>
+        <NavLink
+          to={item.path}
+          className={({ isActive }) =>
+            isActive
+              ? "sidemenu__link  sidemenu__link--active"
+              : "sidemenu__link "
+          }
+        >
+          <>
+            <div className="sidemenu__img">
+              <img src={item.icon} />
+            </div>
+            <div className="sidemenu__text">{item.name}</div>
+          </>
+        </NavLink>
       </div>
     </>
   );
