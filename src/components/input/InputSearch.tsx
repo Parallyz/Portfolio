b4bcc4982c4d124b41f4985741f8af8d86335d12
Parallyz <vladislav.verus@gmail.com>
2022-10-12 12:15:21 +0300

    rework input select

--- src/components/input/InputSearch.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const InputSearch = () => {
-  return <></>;
-};
-
-export default InputSearch;
--- src/components/input/InputSelect.tsx
+++ src/components/input/InputSelect.tsx
@@ -7,44 +7,10 @@ interface InputSelectProps {
   options: any[];
 }
 
-const customStyles = {
-  option: (provided: any, state: any) => ({
-    color: state.isSelected ? "#f7f8fc" : "#3e4049",
-    padding: "7px 10px",
-    backgroundColor: state.isSelected
-      ? "#3e4049"
-      : state.isFocused
-      ? "#a4a6b3"
-      : "#f7f8fc",
-  }),
-  control: () => ({
-    backgroundColor: "#f7f8fc",
-    display: "flex",
-    border: "1px solid #a4a6b3",
-    marginTop: 10,
-    borderRadius: 5,
-  }),
-  menuList: () => ({
-    padding: 0,
-    borderRadius: 5,
-    height: 100,
-    overflow: "auto",
-  }),
-  menu: (provided: any, state: any) => ({
-    ...provided,
-  }),
-  //  singleValue: (provided: any, state: any) => {
-  //    const opacity = state.isDisabled ? 0.5 : 1;
-  //    const transition = "opacity 300ms";
-
-  //    return { ...provided, opacity, transition };
-  //  },
-};
-
 const InputSelect = (props: InputSelectProps) => {
   return (
     <Select
-      styles={customStyles}
+      classNamePrefix="input--select"
       isSearchable={true}
       defaultValue={props.value}
       onChange={props.setValue}
